68df43f65702e116e7373352652dbeaf8ebbfd0b
NTSon <thaisonbk2020@gmail.com>
2022-08-05 18:01:41 +0700

    Finish [36-37] lessons: Sidebar, Searchbox, Search screen

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import { useState } from "react";
+
+function SearchBox() {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate(query ? `/search/?query=${query}` : "/search");
+  };
+
+  return (
+    <Form className="d-flex me-auto" onSubmit={submitHandler}>
+      <InputGroup>
+        <FormControl
+          type="text"
+          name="q"
+          id="q"
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search products..."
+          aria-label="Search Products"
+          aria-describedby="button-search"
+        ></FormControl>
+        <Button variant="outline-primary" type="submit" id="button-search">
+          <i className="fas fa-search"></i>
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+}
+
+export default SearchBox;
