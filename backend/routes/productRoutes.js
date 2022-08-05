9e07f68e0c68a034ae419bdd5efc2971a1a36e9a
NTSon <thaisonbk2020@gmail.com>
2022-08-05 23:14:51 +0700

    Finish [38-41] lessons

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Product from "../models/productModel.js";
 import expressAsyncHandler from "express-async-handler";
+import { isAdmin, isAuth } from "../utils.js";
 
 const productRouter = express.Router();
 
@@ -9,11 +10,59 @@ productRouter.get("/", async (req, res) => {
   res.send(products);
 });
 
+productRouter.post(
+  "/",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const newProduct = new Product({
+      name: "sample name " + Date.now(),
+      slug: "sample-name-" + Date.now(),
+      category: "sample category",
+      image: "/images/p1.jpg", // 679px × 829px
+      price: 0,
+      countInStock: 0,
+      brand: "sample brand",
+      rating: 0,
+      numReviews: 0,
+      description: "sample description",
+    });
+
+    const product = await newProduct.save();
+    res.send({ message: "Product created", product });
+  })
+);
+
 const PAGE_SIZE = 3;
+
+productRouter.get(
+  "/admin",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const { query } = req;
+    const page = query.page || 1;
+    const pageSize = query.pageSize || PAGE_SIZE;
+
+    const products = await Product.find()
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+
+    const countProducts = await Product.countDocuments();
+
+    res.send({
+      products,
+      countProducts,
+      page,
+      pages: Math.ceil(countProducts / pageSize),
+    });
+  })
+);
+
 productRouter.get(
   "/search",
   expressAsyncHandler(async (req, res) => {
-    const {query} = req
+    const { query } = req;
     const pageSize = query.pageSize || PAGE_SIZE;
     const page = query.page || 1;
     const category = query.category || "";
